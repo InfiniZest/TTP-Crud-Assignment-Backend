c4c02753f4e935c862897004def68969ca8b5d9e
Imran Khan <imran.khan2373@gmail.com>
2021-01-21 14:27:28 -0500

    Removed Posterior-Chain-Master since tables were not being created on db

--- posterior-chain-master/posterior-chain-master/database/models/index.js
+++
@@ -1,7 +0,0 @@
-// Here, we can prepare to register our models, set up associations between tables, and generate a barrel file for the models;
-
-const Player = require('./player');
-
-module.exports = {
-  Player
-};
--- posterior-chain-master/posterior-chain-master/utilities/databaseName.js
+++
@@ -1,4 +0,0 @@
-const pkg = require('../package.json');
-const databaseName = pkg.name.toLowerCase();
-
-module.exports = databaseName;
